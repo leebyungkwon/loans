2769566afc7fd378dd6d3132a5284b056cd5a714
a <a@DESKTOP-BSD2S1E>
2021-04-29 14:10:19 +0900

    2021-04-29 함수 추가 commit

--- src/main/java/com/loanscrefia/config/ConfigurationForAop.java
+++ src/main/java/com/loanscrefia/config/ConfigurationForAop.java
@@ -1,22 +1,15 @@
 package com.loanscrefia.config;
 
-import javax.servlet.http.HttpServletRequest;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-
 import com.loanscrefia.common.common.domain.BaseDomain;
 import com.loanscrefia.common.login.domain.SecurityMember;
-import com.loanscrefia.common.member.domain.MemberDomain;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -44,7 +37,7 @@ public class ConfigurationForAop {
 				if(p[0]==null || p[0]=="") return null;
 				domain = (BaseDomain)p[0];
 				domain.setRegSeq(seq);
-				domain.setUpdSeq(seq);	
+				domain.setUpdSeq(seq);
 			}
 		}
 		return domain;
