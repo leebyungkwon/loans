1cebdfe7194b3c70c1a95fd66482624a2e41fb73
sohui <sohui@DESKTOP-OATLJB9>
2021-05-06 15:31:23 +0900

    [유소희]회원사 시스템 > 모집인 등록 : 법인 임원,전문인력,전산인력 관련 VO 추가

---
+++ src/main/java/com/loanscrefia/member/user/domain/UserItDomain.java
@@ -0,0 +1,31 @@
+package com.loanscrefia.member.user.domain;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.type.Alias;
+
+import com.loanscrefia.common.common.domain.BaseDomain;
+import com.loanscrefia.util.excel.ExcelColumn;
+
+import lombok.Data;
+
+@Data
+@Alias("userIt")
+public class UserItDomain extends BaseDomain {
+
+	//전산인력(tb_lc_mas01_it)
+	private int operSeq;			//전산인력시퀀스
+	private int masterSeq;			//접수번호시퀀스
+	
+	@ExcelColumn(headerName="성명", vCell="A", vLenMin=2, vLenMax=20)
+	private String operName;		//이름
+	
+	@ExcelColumn(headerName="주민등록번호", vCell="B", vLenMin=14, vLenMax=14)
+	private String plMZId;			//주민등록번호
+	
+	private int fileSeq;			//첨부파일 그룹 시퀀스
+	
+	//엑셀 업로드
+	private List<Map<String, Object>> excelParam;
+}
