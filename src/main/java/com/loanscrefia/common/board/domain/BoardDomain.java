58bbb3c96aa51ca152b870d7a608c476fba64429
sabio <sabio@kakao.com>
2021-05-15 11:03:43 +0900

    33

--- src/main/java/com/loanscrefia/common/board/domain/BoardDomain.java
+++ src/main/java/com/loanscrefia/common/board/domain/BoardDomain.java
@@ -14,19 +14,19 @@ import lombok.Data;
 public class BoardDomain extends BaseDomain{
 
 	private String 	BoardNo;            	// (?)								// (삭제 가능 ? - templete 에서 사용중)
-	private String 	BoardType;         	// (?)								// (삭제 가능 ? - templete 에서 사용중)
+	private String 	BoardType;         		// (?)								// (삭제 가능 ? - templete 에서 사용중)
 	
-	private Long 	memberSeq;    		// 멤버 		시퀀스
+	private Long 	memberSeq;    			// 멤버 	시퀀스
 	private Long 	noticeSeq;     		 	// 공지사항 시퀀스
-	private String 	title;      				// 제목
-	private String 	info;      					// 내용
-	private int 		viewCnt;         		// 조회수		
-	private int 		fileSeq;         			// 첨부파일 시퀀스
-	private String 	delYn;           			// 삭제여부
-	private int 		regSeq;            	 	// 등록자 	시퀀스
-	private String 	regTimestamp;  	 	// 등록일시		
-	private int 		updSeq;            	 	// 수정자 	시퀀스
-	private String 	updTimestamp;    	// 수정일시	
+	private String 	title;      			// 제목
+	private String 	info;      				// 내용
+	private int 	viewCnt;         		// 조회수		
+	private int 	fileSeq;         		// 첨부파일 시퀀스
+	private String 	delYn;           		// 삭제여부
+	private int 	regSeq;            	 	// 등록자 	시퀀스
+	private String 	regTimestamp;  	 		// 등록일시		
+	private int 	updSeq;            	 	// 수정자 	시퀀스
+	private String 	updTimestamp;    		// 수정일시	
 	
 	List<FileDomain> fileList;
 	
