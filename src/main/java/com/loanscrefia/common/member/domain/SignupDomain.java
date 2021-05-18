2b1d3b21eb1199a2242f580828c275aefc57f91f
tkfkd <tkfkd@DESKTOP-R4P2IPO>
2021-05-18 14:12:35 +0900

    [김중수]

--- src/main/java/com/loanscrefia/common/member/domain/SignupDomain.java
+++ src/main/java/com/loanscrefia/common/member/domain/SignupDomain.java
@@ -28,26 +28,21 @@ public class SignupDomain extends BaseDomain{
 	
 	private int 	comCode;				// 회원사코드
 	
-	@NotBlank(message = "담당자이름 을 입력하세요.")
 	@Pattern(regexp = "^[a-zA-Z가-힣]*${1,20}", message = "담당자이름 은 한글 1~20자리로 입력해 주세요.")
 	private String 	memberName;				// 담당자이름
 	
-	@NotBlank(message = "이메일 을 입력해주세요.")
 	@Email
 	private String 	email;					// 이메일
 	
 	@Pattern(regexp = "^\\d{3}-\\d{3,4}-\\d{4}${1,20}", message = "휴대폰번호를 다시 입력해 주세요.")
 	private String 	mobileNo;				// 휴대폰번호
 	
-	@NotBlank(message = "회사전화번호 를 입력해주세요.")
 	@Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}${1,20}", message = "회사전화번호를 다시 입력해 주세요.")
 	private String 	extensionNo;			// 회사 전화번호
 	
-	@NotBlank(message = "부서명 을 입력해주세요.")
 	@Pattern(regexp = "^[a-zA-Z0-9가-힣]*${1,30}", message = "부서명 은 한글 1~30자리로 입력해 주세요.")
 	private String 	deptNm;					// 부서명
 	
-	@NotBlank(message = "직위명 을 입력해주세요.")
 	@Pattern(regexp = "^[a-zA-Z가-힣]*${1,10}", message = "직위명 은 한글 1~20자리로 입력해 주세요.")
 	private String 	positionNm;				// 직위명
 	
