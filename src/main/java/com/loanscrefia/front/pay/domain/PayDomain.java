e7a8336fe45b69c55335412c940819892f9ae89d
sohui <sohui@DESKTOP-OATLJB9>
2021-05-21 11:31:59 +0900

    [유소희]front -> fo / 결제 소스 추가

---
+++ src/main/java/com/loanscrefia/front/pay/domain/PayDomain.java
@@ -0,0 +1,13 @@
+package com.loanscrefia.front.pay.domain;
+
+import org.apache.ibatis.type.Alias;
+
+import com.loanscrefia.common.common.domain.BaseDomain;
+
+import lombok.Data;
+
+@Data
+@Alias("pay")
+public class PayDomain extends BaseDomain {
+
+}
---
+++ src/main/java/com/loanscrefia/front/pay/repository/PayRepository.java
@@ -0,0 +1,8 @@
+package com.loanscrefia.front.pay.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface PayRepository {
+
+}
