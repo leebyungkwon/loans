5c6b93b244083e4be07ce99306b6d4e700d1798f
sabio <sabio@kakao.com>
2021-06-08 14:40:20 +0900

    2021-06-08 권한변경 commit

--- src/main/java/com/loanscrefia/config/ConfigurationForSecurity.java
+++ src/main/java/com/loanscrefia/config/ConfigurationForSecurity.java
@@ -45,6 +45,7 @@ public class ConfigurationForSecurity extends WebSecurityConfigurerAdapter {
     	
     	// 권한체크/
 		http.authorizeRequests()
+			.antMatchers("/").permitAll()
 			.antMatchers("/front/**").permitAll()
 			.antMatchers("/admin/**").hasAnyRole("ADMIN","SYSTEM")
 			.antMatchers("/member/admin/adminDetailUpdPage").hasAnyRole("TEMP_MEMBER", "MEMBER","SYSTEM")
