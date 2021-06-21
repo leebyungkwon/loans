b0727ec2c20fba9b0093e3f6d34aedda0e7ebbfb
sohui <sohui@DESKTOP-OATLJB9>
2021-06-21 10:45:57 +0900

    [유소희]엔터키 연타 방지

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,38 +1,33 @@
 (function($){
-
-$(document).ready(function(){
-     //레이어팝업 짤림 방지
-
-     $(window).resize(function(){
-       var pop_height = $('.popup_wrap .height').height()+70;
-       var win_height = window.innerHeight;
-       if(pop_height >= win_height){
-         $('.popup_wrap .popup_inner').addClass('resize')
-       }else{
-         $('.popup_wrap .popup_inner').removeClass('resize')
-       }
-     })
-
-     //레이어팝업 짤림 방지
-
-     $('#pop_01').click(function(){
-       $('.popup_wrap').show();
-       $('.popup_wrap .popup_inner').addClass('height');
-     });
-
-     $('.popup_wrap .pop_close').click(function(){
-       $(this).parents('.popup_wrap').hide();
-       $('.popup_wrap .popup_inner').removeClass('height');
-     });
-     
-     
-
-   });
-
-$(document).ready(function(){
-   $('.btn_sort').click(function(){
-     $('.btn_sort').toggleClass('on');
-   });
-});
-
+	$(document).ready(function(){
+		//레이어팝업 짤림 방지
+		$(window).resize(function(){
+			var pop_height = $('.popup_wrap .height').height()+70;
+			var win_height = window.innerHeight;
+			if(pop_height >= win_height){
+				$('.popup_wrap .popup_inner').addClass('resize')
+			}else{
+				$('.popup_wrap .popup_inner').removeClass('resize')
+			}
+		});
+		
+		/*
+		$('.popup_wrap .pop_close').click(function(){
+			$(this).parents('.popup_wrap').hide();
+			$('.popup_wrap .popup_inner').removeClass('height');
+		});
+		*/
+		
+		//승인남은일수 클릭 이벤트
+		$('.btn_sort').click(function(){
+			$('.btn_sort').toggleClass('on');
+		});
+		
+		//엔터키 연타 방지(2021-06-21)
+		$('a').keypress(function(e){
+			if(e.which == 13){
+				e.preventDefault();
+			}
+		});
+	});
 })(jQuery);
