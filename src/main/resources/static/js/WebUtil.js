6c924e80c3940bfe7f7e473751b1facbac168d1f
lbk <lbk@DESKTOP-E4MJGIC>
2021-08-20 14:12:57 +0900

    뒤로가기 - 체크박스 수정

--- src/main/resources/static/js/WebUtil.js
+++ src/main/resources/static/js/WebUtil.js
@@ -137,7 +137,7 @@ var WebUtil = {
 			if(t.isNotNull(selectorName)){
 				var selectorType = $(selector).prop('type');
 				for(k in key){
-					$(".k_search").find("input:checkbox[name='"+selectorName+"']").prop("checked",false);
+					if(selectorType == 'checkbox') $(".k_search").find("input:checkbox[name='"+selectorName+"']").prop("checked",false);
 					
 					if(selectorName == key[k] && selectorType == 'radio') $(".k_search").find("input:radio[name='"+selectorName+"']:radio[value='"+param[key[k]]+"']").prop('checked', true);
 			 		else if(selectorName == key[k] && selectorType == 'checkbox') $(".k_search").find("input:checkbox[name='"+selectorName+"'][value="+param[key[k]]+"]").prop("checked",true);
